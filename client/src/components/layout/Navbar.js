97c9f528fd5e53df1bf15bdeef1fd6568a0cf6be
nitinuttarwar007 <nitin.uttarwar007@gmail.com>
2019-04-30 00:30:51 +0530

    initial commit v1.0.0

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <div className="nav-wrapper white">
+            <Link
+              to="/"
+              style={{
+                fontFamily: "monospace"
+              }}
+              className="col s5 brand-logo center black-text"
+            >
+              <i className="material-icons">how_to_vote</i>
+              Match Bet App
+            </Link>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+export default Navbar;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://nitinut:Feb_2019@vote-app-mffz7.mongodb.net/vote_app",
+    secretOrKey: "secret"
+};
