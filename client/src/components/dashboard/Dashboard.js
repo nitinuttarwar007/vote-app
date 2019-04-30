57d66332524ae640434086bda7a6817ff83f9cb5
Nitin Uttarwar <nitin.uttarwar007@gmail.com>
2019-04-30 16:25:30 +0530

    updated dashboard.js

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -16,8 +16,8 @@ return (
             <h4>
               <b>Hey there,</b> {user.name.split(" ")[0]}
               <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+                You are logged into a {" "}
+                <span style={{ fontFamily: "monospace" }}> Bet </span> app 👏
               </p>
             </h4>
             <button
